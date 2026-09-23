aeb5a735876c041621b8f7b645549f8f1aeaf4bc
Csar090 <sarveshton@gmail.com>
2026-09-23 12:55:39 +0530

    Connect dashboard to the replacement Apps Script deployment

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // This permanent Apps Script deployment URL is intentionally public.
 // Secrets and API keys remain only in Apps Script properties.
 window.FLUENCY_CONFIG = Object.freeze({
-  backendUrl: "https://script.google.com/macros/s/AKfycbyPW2GtteW6eZ8w1z14YfzuJXwTHWwCtkG4petm3FNuvUCaVGOGmuZz1f_0xLvPnaJu/exec"
+  backendUrl: "https://script.google.com/macros/s/AKfycbz8TKlSwIeOT7flxVsASfQHeTSv3sY-qJpEI5eWg1I0vbG6goamrYky_JQJ_Gkgr-8/exec"
 });
